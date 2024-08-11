7d0868f02eb0f7344145bd3076c064b0e794f0da
Muzamiil Ali <shanikbs6242@gmail.com>
2024-08-12 01:17:19 +0500

    new commit

--- src/pages/jobpost/index.js
+++ src/pages/jobpost/index.js
@@ -8,9 +8,15 @@ import jobsData from '../jobs'
 import 'react-select-search/style.css'
 const JobPost = () => {
     const [formData,setFormData]=useState({
-        name:"",
-        email:"",
-        password:""
+        title:"",
+        companyName:"",
+        education:"",
+        jobType:"",
+        salaryFrom:"",
+        salaryTo:"",
+        category:"",
+        description:""
+
       })
      const handleFormChange= (event)=>{
      const {name,value} =event.target;
@@ -18,19 +24,23 @@ const JobPost = () => {
     }
     const handleCancle=()=>{
       setFormData({
-        name:"",
-        email:"",
-        password:""
+        title:"",
+        companyName:"",
+        education:"",
+        jobType:"",
+        salaryFrom:"",
+        salaryTo:"",
+        category:"",
+        description:""
+
       })
     }
     const handleFormSubmit=async(event)=>{
       event.preventDefault()
     
     try{
-      const res = await axios.post("http://localhost:5000/api/auth/signup",{
-        name:formData.name,
-        email:formData.email,
-        password:formData.password
+      const res = await axios.post("http://localhost:5000/api/jobs/post-job",{
+       ...formData
       })
       console.log(res);
       alert("SignUp Successfully");
@@ -46,106 +56,122 @@ const JobPost = () => {
       <div className="w-full px-12 p-8 space-y-8 my-8 bg-white rounded-lg shadow-lg">
         <h2 className="text-2xl font-bold text-center text-gray-900">Post a Job</h2>
         <form className=" gap-y-2 gap-x-4 grid grid-cols-2" onSubmit={handleFormSubmit}>
-          <div className='px-4'>
+          <div className='px-4 col-span-2 md:col-span-1 '>
             <label htmlFor="name" className="block text-sm font-medium text-gray-700">Job Title</label>
             <input
+            required
               type="text"
-              id="name"
-              name='name'
-              value={formData.name}
+              id="title"
+              name='title'
+              value={formData.title}
               placeholder="Enter your full name"
               onChange={handleFormChange}
               className="w-full px-3 py-2 mt-1 text-gray-900 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
-          <div className='px-4'>
+          <div className='px-4 col-span-2 md:col-span-1'>
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">Company Name</label>
             <input
-              type="email"
-              id="email"
-              name='email'
-              value={formData.email}
+            required
+              type="text"
+              id="companyName"
+              name='companyName'
+              value={formData.companyName}
               onChange={handleFormChange}
-              placeholder="Enter your email"
+              placeholder="Enter Company Name"
               className="w-full px-3 py-2 mt-1 text-gray-900 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
-          <div className='px-4'>
+          <div className='px-4 col-span-2 md:col-span-1'>
             <label htmlFor="password" className="block text-sm font-medium text-gray-700">Employee Education </label>
             <select
-              type="password"
+            required
+              
               id="confirm-password"
-              placeholder="Confirm your password"
+              placeholder="Select Required Education"
+              name='education'
+              onChange={handleFormChange}
+              value={formData.education}
               className="w-full px-3 py-2 mt-1 text-gray-900 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             >
-                <option>Middle</option>
-                <option>Matric</option>
-                <option>Intermediate</option>
-                <option>Bachlors</option>
-                <option>Masters</option>
+                <option value="middle">Middle</option>
+                <option value="matric">Matric</option>
+                <option value="intermediate">Intermediate</option>
+                <option value="bachlors">Bachlors</option>
+                <option value="masters">Masters</option>
                 </select>
           </div>
-          <div className='px-4'>
+          <div className='px-4 col-span-2 md:col-span-1'>
             <label htmlFor="confirm-password" className="block text-sm font-medium text-gray-700">Job Type</label>
             <select
-              type="password"
-              id="confirm-password"
-              placeholder="Confirm your password"
+            required
+              
+              id="jobType"
+              placeholder="Select Job Type"
+              name='jobType'
+              value={formData.jobType}
+              onChange={handleFormChange}
               className="w-full px-3 py-2 mt-1 text-gray-900 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             >
-                <option>Fulltime</option>
-                <option>Parttime</option>
-                <option>Remote</option>
+                <option value="fullTime">Fulltime</option>
+                <option value="partTime">Parttime</option>
+                <option value="remote">Remote</option>
                 </select>
           </div>
-          <div className='px-4'>
+          <div className='px-4 col-span-2 md:col-span-1'>
           <label htmlFor="email" className="block text-sm font-medium text-gray-700">Salary Range </label>
           <div className='grid grid-cols-2 gap-x-4'>
           <input
+          required
           type='number'
+          name='salaryFrom'
+          value={formData.salaryFrom}
+          onChange={handleFormChange}
           placeholder='From'
            className=" px-3 py-2 mt-1 text-gray-900 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           />
           <input
+          required
           type='number'
+          name='salaryTo'
+          onChange={handleFormChange}
+          value={formData.salaryTo}
           placeholder='To'
            className=" px-3 py-2 mt-1 text-gray-900 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           />
           </div>
           </div>
-          <div className='px-4'>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Employee Education </label>
-            <select
-              type="password"
-              id="confirm-password"
-              placeholder="Confirm your password"
-              className="w-full px-3 py-2 mt-1 text-gray-900 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            >
-                <option>Middle</option>
-                <option>Matric</option>
-                <option>Intermediate</option>
-                <option>Bachlors</option>
-                <option>Masters</option>
-                </select>
-          </div>
-          <div className='px-4'>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700"> Category </label>
-            <SelectSearch options={jobsData}  value="sv" placeholder='choose Category' onChange={(e)=>{console.log(e)}} name="category"/>
-          </div>
           
+          <div className='px-4 col-span-2 md:col-span-1'>
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700"> Category </label>
+            <span className='bg-gray-400 w-full'>
+            <SelectSearch options={jobsData} name="" value={formData.category} placeholder='choose Category' onChange={(e)=>{setFormData((ep)=>({...ep,category:e}))}} />
+            </span>
+          </div>
+          <div className='px-4   col-span-2'>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700"> Job Description</label>
+            <textarea
+              type="email"
+              id="email"
+              name='description'
+              rows="3"
+              value={formData.description}
+              onChange={handleFormChange}
+              placeholder="Enter Full Job Descrption"
+              className="w-full px-3 py-2 mt-1 text-gray-900 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div className='flex justify-center bg-slate-500 col-span-2'>
           <button
+          
             type="submit"
-            className="w-full px-4 py-2 text-sm col-span-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className=" px-4 w-full   py-2 text-sm  font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
-            Sign Up
+            Post Job
           </button>
+          </div>
         </form>
-        <p className="text-sm text-center text-gray-600">
-          Already have an account?{' '}
-          <Link href="/login">
-            <span className="font-medium text-indigo-600 hover:text-indigo-500">Login</span>
-          </Link>
-        </p>
+
       </div>
     </div>
   )
